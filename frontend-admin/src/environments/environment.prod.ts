bfdd92dc08b65f5d3a695de3677ab7287ae42100
VinayakChavda <vchavda@buchanan.com>
2026-04-17 12:15:36 +0530

    fixed websocket issue

--- frontend-admin/src/environments/environment.prod.ts
+++ frontend-admin/src/environments/environment.prod.ts
@@ -3,5 +3,5 @@ export const environment = {
   production: true,
   tablesQrURL: 'http://54.90.221.180',
   backendUrl: 'http://54.90.221.180/api',
-  socketIp : "54.90.221.180/api"
+  socketIp : "54.90.221.180"
 };
